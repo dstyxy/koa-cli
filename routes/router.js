9e0d62c3e074c4cf156a634bf8f13a8e330104af
dstyxy <dstyxy@qq.com>
2024-03-27 16:06:35 +0800

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,12 @@
 let Router = require('koa-router')
 
 let home = require('./home.js')
-let video = require('./video.js')
+
 let router = new Router()
 
 
 router.use('/home', home.routes(), home.allowedMethods())
-router.use('/video', video.routes(), video.allowedMethods())
+
 
 
 module.exports = router
